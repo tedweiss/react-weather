968eb8ab1c7897cccd4d40acf1a48dd319d46ad1
Ted Weiss <theodore_weiss@outlook.com>
2019-07-06 23:01:15 -0400

    add Forecast component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Current from './current/Current'
+import Forecast from './forecast/Forecast'
 
 const App = () => {
   return (
     <>
       <Current />
+      <Forecast />
     </>
   )
 }
