d1f8a81d06deb7bcda7eee9220eaf0231b8a91aa
Ted Weiss <theodore_weiss@outlook.com>
2019-07-06 23:34:14 -0400

    add buttons to switch pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Current from './current/Current'
 import Forecast from './forecast/Forecast'
 
 const App = () => {
+  const [page, setPage] = useState('current')
+  const handleClick = clickedPage => {
+    setPage(clickedPage)
+  }
   return (
     <>
-      <Current />
-      <Forecast />
+      <button
+        className={'current-button'}
+        onClick={() => {
+          handleClick('current')
+        }}>
+        Current
+      </button>
+      <button
+        className={'forecast-button'}
+        onClick={() => {
+          handleClick('forecast')
+        }}>
+        Forecast
+      </button>
+      {page === 'current' && <Current />}
+      {page === 'forecast' && <Forecast />}
     </>
   )
 }
